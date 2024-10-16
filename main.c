9239c944471ab66c7df7cf4eb0e4c9c12624e99c
Daniel Velasquez <danivelas4@gmail.com>
2024-10-15 22:17:40 -0400

    starting to understand gbdk funcs M main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <gb/gb.h>
 
 unsigned char smilerTiles[] = {
-    // first tile
+    // frowning tile
     0x7E,
     0x7E,
     0xFF,
@@ -18,7 +18,7 @@ unsigned char smilerTiles[] = {
     0x81,
     0x7E,
     0x7E,
-    // second tile
+    // smiling tile
     0x7E,
     0x7E,
     0xFF,
@@ -43,8 +43,24 @@ unsigned char smilerSpritesMap[] = {
 };
 
 void main(void) {
+  UINT8 frowning_sprite_index = 0;
+  UINT8 smiling_sprite_index = 1;
+
+  // read all tiles into 0th position at VRAM
   set_sprite_data(0, 2, smilerTiles);
-  set_sprite_tile(0, 0);
-  move_sprite(0, 88, 78);
+
+  // write frowning_sprite_index and smiling_sprite_index to 0th and 1st
+  // positions in the OAM resp.
+  set_sprite_tile(frowning_sprite_index, frowning_sprite_index);
+  set_sprite_tile(smiling_sprite_index, smiling_sprite_index);
+
+  // place sprites
+  move_sprite(frowning_sprite_index, 50, 50);
+  move_sprite(smiling_sprite_index, 70, 70);
   SHOW_SPRITES;
+
+  // while (1) {
+  //   sprite_index = 1 ? sprite_index == 0 : 0;
+  //   set_sprite_tile(0, sprite_index);
+  // }
 }
